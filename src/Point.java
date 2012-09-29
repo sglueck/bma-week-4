614535a448f8d8a52025528063fec0b038ede73f
dfarley <david@crossfireconsulting.com>
2012-09-29 14:51:58 -0400

    overrode Point constructor with no-argument version

--- src/Point.java
+++ src/Point.java
@@ -1,6 +1,7 @@
 public class Point {
 	public static void main(String[] args) {
-		Point point = new Point(10, 33);
+		Point point1 = new Point(10, 33);
+		Point point2 = new Point();
 	}
 	
 	int x;
@@ -9,5 +10,8 @@ public class Point {
 	Point(int x, int y) {
 		this.x = x;
 		this.y = y;
+	}
+	
+	Point() {
 	}
 }
