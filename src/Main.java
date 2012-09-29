d11fd75c4d64f544fcc2d16ecbb9ba56bd8a481f
dfarley <david@crossfireconsulting.com>
2012-09-29 14:04:59 -0400

    altered getHelloMessage() to use a static variable

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,14 @@
 
 public class Main {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		String message = getHelloMessage();
 		System.out.println(message);
 	}
 	
+	static String helloMessage = "Hello World";
+	
 	static String getHelloMessage() {
-		return "Hello World";
+		return helloMessage;
 	}
 
 }
