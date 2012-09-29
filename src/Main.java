29e3a361410e5fbc629c2bf81b07fe64f832222a
dfarley <david@crossfireconsulting.com>
2012-09-29 13:52:20 -0400

    added 'static String getHelloMessage()'

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,13 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String message = getHelloMessage();
+		System.out.println(message);
+	}
+	
+	static String getHelloMessage() {
+		return "Hello World";
 	}
 
 }
+
